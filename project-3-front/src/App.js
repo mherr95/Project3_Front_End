f75913d7a9d4300892bdbc08382e02327c74f026
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-06-14 11:11:36 -0400

    added two urls and started get songs function

--- project-3-front/src/App.js
+++ project-3-front/src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 
+const songsURL = 'http://localhost:3003/songs'
+const moviesURL = 'http://localhost:3003/movies'
+
 export default class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      songs: [],
+      movies: []
+    }
+  }
+
+  getSongs() {
+    fetch(songsURL)
+    .then(res => {return res.json()})
+    .then(data => this.setState({songs: data}))
+  }
+
+
   render() {
     return (
       <div>
