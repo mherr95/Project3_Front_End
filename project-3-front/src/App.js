af1a312353ab603c45bbd148c4d1396dda21ab8b
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2021-06-14 11:43:46 -0400

    added SongForm component

--- project-3-front/src/App.js
+++ project-3-front/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Movieform from './components/Movieform'
+import SongForm from './components/SongForm'
 
 
 const songsURL = 'http://localhost:3003/songs'
@@ -15,6 +16,20 @@ export default class App extends Component {
     }
   }
 
+
+  /////////////////
+  // SONGS CODE
+  ////////////////
+
+  handleAddSong (song) {
+    const copySongs = [...this.state.songs]
+    copySongs.unshift(song)
+    this.setState({
+      songs: copySongs,
+      name: ''
+    })
+  }
+
   getSongs() {
     fetch(songsURL)
     .then(res => {return res.json()})
@@ -26,9 +41,10 @@ export default class App extends Component {
     return (
       <div>
         <h1>My favorite things</h1>
-        <Movieform />
         <h3>Favorite Songs</h3>
+        <SongForm handleAddSong = {this.handleAddSong} />
         <h3>Favorite Movies</h3>
+        <Movieform />
       </div>
     )
   }
